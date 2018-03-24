301ca5529b981406d444b465bdb6c1a97d88a05b
Boris Resnick <boris@resnick.ru>
2018-03-24 11:40:43 +0300

    Split sasm code

---
+++ src/fp.py
@@ -0,0 +1,209 @@
+
+import struct
+import logging as lg
+
+import ops
+
+# Struct definition
+class Struct:
+    def __init__(self, name):
+        self.name = name
+        self.fields = dict()
+        self.size = 0                       # size in 4-byte words
+        
+    def add_field(self, name, width):
+        lg.debug("Field {0}:{1}:{2}".format(name, width, self.size))
+        self.fields[name] = self.size
+        self.size += width
+        
+    def get_offset(self, fieldname):
+        return self.fields[fieldname] * 4   # offset in bytes
+
+# First pass processor
+class FPP:
+    def __init__(self):      
+        self.cmd_list = list()
+        self.offset = 0
+        self.namespace = "<global>"
+        self.label_dict = dict()
+        self.context = None
+        self.structs = dict()
+        
+    def get_qualified_name(self, name, namespace = None):    
+        if(namespace == None):
+            namespace = self.namespace    
+        return namespace + "::" + name
+        
+    def resolve_name(self, tokens):
+        if(len(tokens) == 1):
+            # Unqualified
+            name = self.get_qualified_name(tokens[0])
+        else:
+            # Qualified
+            name = self.get_qualified_name(tokens[1], tokens[0]) # name, namespace        
+            
+        return name
+
+    # Handlers
+    def issue_word(self, fmt, word):
+        bytestr = struct.pack(fmt, word)
+        self.cmd_list.append(('bytes', bytestr))
+        self.offset += 4
+
+    def issue_usigned(self, word):        
+        self.issue_word(">I", word)
+
+    def issue_signed(self, word):
+        self.issue_word(">i", word)
+
+    def issue_op(self, op):
+        lg.debug("Issuing command 0x{0:X}".format(op))
+        self.issue_usigned(op)
+        
+    def on_uconst(self, tokens):
+        word = int(tokens[0])
+        self.issue_usigned(word)
+        
+    def on_sconst(self, tokens):
+        word = int(tokens[0])
+        self.issue_signed(word)        
+
+    def on_label(self, tokens):
+        labelname = tokens[0]
+        qlabelname = self.get_qualified_name(labelname)
+        self.label_dict[qlabelname] = self.offset
+        lg.debug("Label {0} @ 0x{1:X}".format(qlabelname, self.offset))
+
+    def on_reg(self, val):
+        self.issue_usigned(val)
+    
+    def on_ref(self, tokens):
+        labelname = self.resolve_name(tokens[0])
+        lg.debug("Ref {0}".format(labelname))
+        
+        current_offset = self.offset
+        self.cmd_list.append(('ref', (current_offset, labelname)))
+        self.offset += 4 # placeholder-bytes        
+
+    def on_fail(self, r):
+        raise Exception("Unknown command {0}".format(r))
+    
+    # Macros
+    def issue_macro_dw(self, tokens):
+        self.on_label([tokens[0]])
+        
+        if(len(tokens) == 2):
+            multipicity = int(tokens[1])
+        else:
+            multipicity = 1
+        
+        for _ in range(multipicity):
+            self.issue_usigned(0x00000000)
+        
+    def issue_macro_call(self, tokens):
+        self.issue_op(ops.ldr)
+        self.on_ref(tokens)
+        self.on_reg(7)               # Using the last for routine address
+        self.issue_op(ops.cll)
+        self.on_reg(7)
+    
+    def issue_macro_func(self, tokens):
+        self.on_label(tokens)        # Does nothing fancy really
+        
+    def macro_begin_struct(self, tokens):
+    
+        name = tokens[0]
+        qname = self.get_qualified_name(name)
+        
+        if(self.context != None):
+            raise Exception("Bad context")
+        self.context = Struct(qname)
+        
+    def macro_struct_field(self, tokens):
+        type = tokens[0]
+        fname = tokens[1]
+        
+        if(type == "DW"):
+            width = 1
+        else:
+            raise Exception("Bad type {0}".format(type))
+        
+        s = self.context
+        if(s == None):
+            raise Exception("Bad context")
+            
+        s.add_field(fname, width)        
+        
+    def macro_struct_end(self, tokens):    
+        s = self.context
+        if(self.context == None):
+            raise Exception("Bad context")
+            
+        self.structs[s.name] = s
+        self.context = None
+        lg.debug("Struct {0}".format(s.name))
+        
+    def issue_macro_ds(self, tokens):
+        qsname = self.resolve_name(tokens[0])        
+        s = self.structs[qsname]
+    
+        name = tokens[1]
+        
+        if(len(tokens) == 3):
+            multipicity = int(tokens[2])
+        else:
+            multipicity = 1
+            
+        words = s.size * multipicity
+        self.on_label([name])
+        # Issue placeholder-bytes
+        for _ in range(words):
+            self.issue_usigned(0x00000000)
+            
+    def issue_macro_ptr_head(self, tokens):
+        self.issue_op(ops.mrr)
+    
+    def issue_macro_rptr_head(self, tokens):
+        self.issue_op(ops.mmr)
+    
+    # Invalidates g, h
+    def issue_macro_ptr_tail(self, tokens):
+        # before: partial command to to load struct address to reg
+        # for PTR: mrr source-reg
+        # for RPTR: mrm source-reg        
+        fname = tokens[0]
+        qsname = self.resolve_name(tokens[1])
+        s = self.structs[qsname]
+        offset = s.get_offset(fname)        
+            
+        self.on_reg(7)               # Using the last ('h') for struct address
+        self.issue_op(ops.ldc)       # Loading the offset
+        self.issue_usigned(offset)
+        self.on_reg(6)               # Using 'g' for field offset
+        self.issue_op(ops.add)       # Adding the offset
+        self.on_reg(7)
+        self.on_reg(6)
+        # after: target reg
+    
+    # Invalidates a, b, h
+    # Returns a
+    def issue_macro_item(self, tokens):
+        # a - base
+        # b - index
+        qsname = self.resolve_name(tokens[0])
+        s = self.structs[qsname]
+        width = s.size*4
+        # ldc <size> h
+        self.issue_op(ops.ldc)
+        self.issue_usigned(width)
+        self.on_reg(7)
+        # mul b h b
+        self.issue_op(ops.mul)
+        self.on_reg(1)
+        self.on_reg(7)
+        self.on_reg(1)
+        # add a b a
+        self.issue_op(ops.add)
+        self.on_reg(0)
+        self.on_reg(1)
+        self.on_reg(0)
---
+++ src/grammar.py
@@ -0,0 +1,152 @@
+import pyparsing as pp
+
+import ops
+from fp import FPP
+
+# Grammar
+def g_cmd(literal, op):
+    return pp.Literal(literal).setParseAction(lambda _: (FPP.issue_op, op))
+
+def g_reg(reg_name, reg_num):
+    return pp.Literal(reg_name).setParseAction(lambda _: (FPP.on_reg, reg_num))
+
+
+comment = pp.Suppress(pp.Literal("//") + pp.SkipTo("\n"))
+
+label = (pp.Word(pp.alphas) + pp.Suppress(':')).setParseAction(lambda r: (FPP.on_label, r))
+
+reg = pp.Or([
+    g_reg('a', 0),
+    g_reg('b', 1),
+    g_reg('c', 2),
+    g_reg('d', 3),
+    g_reg('e', 4),
+    g_reg('f', 5),
+    g_reg('g', 6),
+    g_reg('h', 7)
+])
+
+def g_cmd_1(literal, op):
+    return g_cmd(literal, op) + reg
+    
+def g_cmd_2(literal, op):
+    return g_cmd(literal, op) + reg + reg
+
+def g_cmd_3(literal, op):
+    return g_cmd(literal, op) + reg + reg + reg
+
+us_dec_const = pp.Regex("[0-9]+").setParseAction(lambda r: (FPP.on_uconst, r))    
+us_const = us_dec_const
+s_const = pp.Regex("[\+\-]?[0-9]+").setParseAction(lambda r: (FPP.on_sconst, r))
+    
+refname = pp.Optional(pp.Word(pp.alphas) + pp.Suppress("::")) + pp.Word(pp.alphas)
+refname.setParseAction(lambda r: [r[:]])  # Join tokens
+ref = (pp.Suppress("&") + refname).setParseAction(lambda r: (FPP.on_ref, r))
+
+# Basic instructions
+hlt_cmd = g_cmd("hlt", ops.hlt)
+nop_cmd = g_cmd("nop", ops.nop)
+jmp_cmd = g_cmd_1("jmp", ops.jmp)
+ldc_cmd = g_cmd("ldc", ops.ldc) + us_dec_const + reg
+mrm_cmd = g_cmd_2("mrm", ops.mrm)
+mmr_cmd = g_cmd_2("mmr", ops.mmr)
+out_cmd = g_cmd_2("out", ops.out)
+jgt_cmd = g_cmd_2("jgt", ops.jgt)
+opn_cmd = g_cmd("opn", ops.opn)
+cls_cmd = g_cmd("cls", ops.cls)
+ldr_cmd = g_cmd("ldr", ops.ldr) + ref + reg
+lsp_cmd = g_cmd_1("lsp", ops.lsp)
+psh_cmd = g_cmd_1("push", ops.psh)
+pop_cmd = g_cmd_1("pop", ops.pop)
+int_cmd = g_cmd_1("int", ops.int)
+cll_cmd = g_cmd_1("cll", ops.cll)
+ret_cmd = g_cmd("ret", ops.ret)
+irx_cmd = g_cmd("irx", ops.irx)
+ssp_cmd = g_cmd_1("ssp", ops.ssp)
+mrr_cmd = g_cmd_2("mrr", ops.mrr)
+
+# Arithmetic
+inv_cmd = g_cmd_2("inv", ops.inv)
+add_cmd = g_cmd_3("add", ops.add)
+sub_cmd = g_cmd_3("sub", ops.sub)
+mul_cmd = g_cmd_3("mul", ops.mul)
+div_cmd = g_cmd_3("div", ops.div)
+mod_cmd = g_cmd_3("mod", ops.mod)
+rsh_cmd = g_cmd_3("rsh", ops.rsh)
+lsh_cmd = g_cmd_3("lsh", ops.lsh)
+bor_cmd = g_cmd_3("or", ops.bor)
+xor_cmd = g_cmd_3("xor", ops.xor)
+band_cmd = g_cmd_3("and", ops.band)
+
+# Emulated
+bpt_cmd = g_cmd("bpt", ops.bpt) + us_dec_const
+
+# Macros
+multi = pp.Optional(pp.Suppress("*") + pp.Regex("[1-9][0-9]*"))
+macro_dw = (pp.Suppress("DW") + pp.Word(pp.alphas) + multi).setParseAction(lambda r: (FPP.issue_macro_dw, r))
+macro_call = (pp.Suppress("CALL") + refname).setParseAction(lambda r: (FPP.issue_macro_call, r))
+macro_func = (pp.Suppress("FUNC") + pp.Word(pp.alphas)).setParseAction(lambda r: (FPP.issue_macro_func, r))
+
+# Macro-struct
+struct_begin = (pp.Suppress("STRUCT") + pp.Word(pp.alphas)).setParseAction(lambda r: (FPP.macro_begin_struct, r))
+field_type = pp.Or(pp.Literal("DW"))
+struct_field = (field_type + pp.Word(pp.alphas)).setParseAction(lambda r: (FPP.macro_struct_field, r))
+struct_end = pp.Suppress("END").setParseAction(lambda r: (FPP.macro_struct_end, r))
+macro_struct = struct_begin + pp.OneOrMore(struct_field) + struct_end
+
+macro_ds = (pp.Suppress("DS") + refname + pp.Word(pp.alphas) + multi).setParseAction(lambda r: (FPP.issue_macro_ds, r))
+
+ptr_head = pp.Literal("PTR").setParseAction(lambda r: (FPP.issue_macro_ptr_head, r))
+rptr_head = pp.Literal("RPTR").setParseAction(lambda r: (FPP.issue_macro_rptr_head, r))
+ptr_tail = (pp.Word(pp.alphas) + pp.Suppress("#") + refname).setParseAction(lambda r: (FPP.issue_macro_ptr_tail, r))
+macro_ptr = ptr_head + reg + ptr_tail + reg
+macro_rptr = rptr_head + reg + ptr_tail + reg
+
+macro_item = (pp.Suppress("ITEM") + refname).setParseAction(lambda r: (FPP.issue_macro_item, r))
+
+# Fail on unknown command
+unknown = pp.Regex(".+").setParseAction(lambda r: (FPP.on_fail, r))
+
+cmd = hlt_cmd \
+    ^ nop_cmd \
+    ^ jmp_cmd \
+    ^ ldc_cmd \
+    ^ mrm_cmd \
+    ^ mmr_cmd \
+    ^ out_cmd \
+    ^ jgt_cmd \
+    ^ opn_cmd \
+    ^ cls_cmd \
+    ^ ldr_cmd \
+    ^ lsp_cmd \
+    ^ psh_cmd \
+    ^ pop_cmd \
+    ^ int_cmd \
+    ^ cll_cmd \
+    ^ ret_cmd \
+    ^ irx_cmd \
+    ^ bpt_cmd \
+    ^ ssp_cmd \
+    ^ inv_cmd \
+    ^ add_cmd \
+    ^ sub_cmd \
+    ^ mul_cmd \
+    ^ div_cmd \
+    ^ mod_cmd \
+    ^ rsh_cmd \
+    ^ lsh_cmd \
+    ^ bor_cmd \
+    ^ xor_cmd \
+    ^ band_cmd \
+    ^ mrr_cmd \
+    ^ macro_dw \
+    ^ macro_call \
+    ^ macro_func \
+    ^ macro_struct \
+    ^ macro_ds \
+    ^ macro_ptr \
+    ^ macro_rptr \
+    ^ macro_item
+    
+statement = pp.Optional(label) + pp.Optional(comment) + cmd + pp.Optional(comment)
+program = pp.ZeroOrMore(statement ^ comment ^ unknown)
