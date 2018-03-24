9fcf2630588fadf1b954b6a2504fa16e9f6a3870
Boris Resnick <boris@resnick.ru>
2018-03-24 11:43:07 +0300

    Refactored grammar (includes underscore in id)

--- src/grammar.py
+++ src/grammar.py
@@ -11,9 +11,10 @@ def g_reg(reg_name, reg_num):
     return pp.Literal(reg_name).setParseAction(lambda _: (FPP.on_reg, reg_num))
 
 
+id = pp.Word(pp.alphas + "_")
 comment = pp.Suppress(pp.Literal("//") + pp.SkipTo("\n"))
 
-label = (pp.Word(pp.alphas) + pp.Suppress(':')).setParseAction(lambda r: (FPP.on_label, r))
+label = (id + pp.Suppress(':')).setParseAction(lambda r: (FPP.on_label, r))
 
 reg = pp.Or([
     g_reg('a', 0),
@@ -39,7 +40,7 @@ us_dec_const = pp.Regex("[0-9]+").setParseAction(lambda r: (FPP.on_uconst, r))
 us_const = us_dec_const
 s_const = pp.Regex("[\+\-]?[0-9]+").setParseAction(lambda r: (FPP.on_sconst, r))
     
-refname = pp.Optional(pp.Word(pp.alphas) + pp.Suppress("::")) + pp.Word(pp.alphas)
+refname = pp.Optional(id + pp.Suppress("::")) + id
 refname.setParseAction(lambda r: [r[:]])  # Join tokens
 ref = (pp.Suppress("&") + refname).setParseAction(lambda r: (FPP.on_ref, r))
 
@@ -83,22 +84,22 @@ bpt_cmd = g_cmd("bpt", ops.bpt) + us_dec_const
 
 # Macros
 multi = pp.Optional(pp.Suppress("*") + pp.Regex("[1-9][0-9]*"))
-macro_dw = (pp.Suppress("DW") + pp.Word(pp.alphas) + multi).setParseAction(lambda r: (FPP.issue_macro_dw, r))
+macro_dw = (pp.Suppress("DW") + id + multi).setParseAction(lambda r: (FPP.issue_macro_dw, r))
 macro_call = (pp.Suppress("CALL") + refname).setParseAction(lambda r: (FPP.issue_macro_call, r))
-macro_func = (pp.Suppress("FUNC") + pp.Word(pp.alphas)).setParseAction(lambda r: (FPP.issue_macro_func, r))
+macro_func = (pp.Suppress("FUNC") + id).setParseAction(lambda r: (FPP.issue_macro_func, r))
 
 # Macro-struct
-struct_begin = (pp.Suppress("STRUCT") + pp.Word(pp.alphas)).setParseAction(lambda r: (FPP.macro_begin_struct, r))
+struct_begin = (pp.Suppress("STRUCT") + id).setParseAction(lambda r: (FPP.macro_begin_struct, r))
 field_type = pp.Or(pp.Literal("DW"))
-struct_field = (field_type + pp.Word(pp.alphas)).setParseAction(lambda r: (FPP.macro_struct_field, r))
+struct_field = (field_type + id).setParseAction(lambda r: (FPP.macro_struct_field, r))
 struct_end = pp.Suppress("END").setParseAction(lambda r: (FPP.macro_struct_end, r))
 macro_struct = struct_begin + pp.OneOrMore(struct_field) + struct_end
 
-macro_ds = (pp.Suppress("DS") + refname + pp.Word(pp.alphas) + multi).setParseAction(lambda r: (FPP.issue_macro_ds, r))
+macro_ds = (pp.Suppress("DS") + refname + id + multi).setParseAction(lambda r: (FPP.issue_macro_ds, r))
 
 ptr_head = pp.Literal("PTR").setParseAction(lambda r: (FPP.issue_macro_ptr_head, r))
 rptr_head = pp.Literal("RPTR").setParseAction(lambda r: (FPP.issue_macro_rptr_head, r))
-ptr_tail = (pp.Word(pp.alphas) + pp.Suppress("#") + refname).setParseAction(lambda r: (FPP.issue_macro_ptr_tail, r))
+ptr_tail = (id + pp.Suppress("#") + refname).setParseAction(lambda r: (FPP.issue_macro_ptr_tail, r))
 macro_ptr = ptr_head + reg + ptr_tail + reg
 macro_rptr = rptr_head + reg + ptr_tail + reg
 
