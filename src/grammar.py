5fc0f0d47fdc08d603d92eeb940b368f29a91f42
Boris Resnick <boris@resnick.ru>
2018-08-05 12:17:48 +0300

    Refactoring: separated basic and macro mechanism

--- src/grammar.py
+++ src/grammar.py
@@ -1,7 +1,7 @@
 import pyparsing as pp
 
 import ops
-from fp import MacroFPP as FPP
+from fpp import FPP
 
 # Grammar
 def g_cmd(literal, op):
@@ -9,7 +9,6 @@ def g_cmd(literal, op):
 
 def g_reg(reg_name, reg_num):
     return pp.Literal(reg_name).setParseAction(lambda _: (FPP.on_reg, reg_num))
-
 
 id = pp.Word(pp.alphas + "_")
 comment = pp.Suppress(pp.Literal("//") + pp.SkipTo("\n"))
@@ -84,35 +83,7 @@ band_cmd = g_cmd_3("and", ops.band)
 bpt_cmd = g_cmd(".break", ops.bpt) + us_dec_const
 aeq_cmd = g_cmd(".assert", ops.aeq) + reg + us_dec_const
 
-# Macros
-multi = pp.Optional(pp.Suppress("*") + pp.Regex("[1-9][0-9]*"))
-macro_dw = (pp.Suppress("DW") + id + multi).setParseAction(lambda r: (FPP.macro_issue_dw, r))
-macro_call = (pp.Suppress("CALL") + refname).setParseAction(lambda r: (FPP.macro_issue_call, r))
-macro_func = (pp.Suppress("FUNC") + id).setParseAction(lambda r: (FPP.macro_issue_func, r))
-
-# Macro-struct
-struct_begin = (pp.Suppress("STRUCT") + id).setParseAction(lambda r: (FPP.macro_begin_struct, r))
-field_type = pp.Or(pp.Literal("DW"))
-struct_field = (field_type + id).setParseAction(lambda r: (FPP.macro_struct_field, r))
-struct_end = pp.Suppress("END").setParseAction(lambda r: (FPP.macro_struct_end, r))
-macro_struct = struct_begin + pp.OneOrMore(struct_field) + struct_end
-
-macro_ds = (pp.Suppress("DS") + refname + id + multi).setParseAction(lambda r: (FPP.macro_issue_ds, r))
-
-ptr_head = pp.Literal("PTR").setParseAction(lambda r: (FPP.macro_issue_ptr_head, r))
-rptr_head = pp.Literal("RPTR").setParseAction(lambda r: (FPP.macro_issue_rptr_head, r))
-ptr_tail = (id + pp.Suppress("#") + refname).setParseAction(lambda r: (FPP.macro_issue_ptr_tail, r))
-macro_ptr = ptr_head + reg + ptr_tail + reg
-macro_rptr = rptr_head + reg + ptr_tail + reg
-
-macro_item = (pp.Suppress("ITEM") + refname).setParseAction(lambda r: (FPP.macro_issue_item, r))
-
-macro_dt = (pp.Suppress("DT") + id + pp.QuotedString('"')).setParseAction(lambda r: (FPP.macro_issue_dt, r))
-
-# Fail on unknown command
-unknown = pp.Regex(".+").setParseAction(lambda r: (FPP.on_fail, r))
-
-cmd = hlt_cmd \
+asm_cmd = hlt_cmd \
     ^ nop_cmd \
     ^ jmp_cmd \
     ^ ldc_cmd \
@@ -144,17 +115,5 @@ cmd = hlt_cmd \
     ^ band_cmd \
     ^ mrr_cmd \
     ^ lla_cmd \
-    ^ macro_dw \
-    ^ macro_call \
-    ^ macro_func \
-    ^ macro_struct \
-    ^ macro_ds \
-    ^ macro_ptr \
-    ^ macro_rptr \
-    ^ macro_item \
-    ^ macro_dt \
     ^ bpt_cmd \
     ^ aeq_cmd
-    
-statement = pp.Optional(label) + pp.Optional(comment) + cmd + pp.Optional(comment)
-program = pp.ZeroOrMore(statement ^ comment ^ unknown)
--- src/semuasm.py
+++ src/semuasm.py
@@ -5,8 +5,8 @@ import os
 import logging as lg
 import struct
 
-import fp
-import grammar
+import mfpp
+import mgrammar
 
 def namespace(in_filename):
     basename = os.path.basename(in_filename)
@@ -15,11 +15,11 @@ def namespace(in_filename):
 
 def compile(in_filenames, out_filename):    
     # First pass
-    first_pass = fp.MacroFPP()        
+    first_pass = mfpp.MacroFPP()        
     for in_filename in in_filenames:
         lg.info("Processing {0}".format(in_filename))
         first_pass.namespace = namespace(in_filename)
-        actions = grammar.program.parseFile(in_filename)
+        actions = mgrammar.program.parseFile(in_filename)
         for (func, arg) in actions:
             func(first_pass, arg)
     
