51b5c38daca79e781e9d4a4a9c35f7a4f8913846
Boris Resnick <boris@resnick.ru>
2018-10-19 23:22:43 +0200

    extra sample removed

--- examples/consume-produce/make.sh
+++
@@ -1,6 +0,0 @@
-SEMU_ROOT="../.."
-ASM=${SEMU_ROOT}"/src/semuasm.py"
-ROM=${SEMU_ROOT}"/roms/consume-produce.bin"
-LIB="../lib/kernel"
-
-py ${ASM} ${LIB}/startup.sasm ${LIB}/kernel.sasm ${LIB}/api.sasm app.sasm ${ROM}
