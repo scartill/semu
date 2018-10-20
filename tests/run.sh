e8392920bc80f217926cb26acd41636f3ff13d3a
Boris Resnick <boris@resnick.ru>
2018-10-20 17:55:22 +0200

    Multifile test support

--- tests/run.sh
+++ tests/run.sh
@@ -1,18 +1,23 @@
 SEMU_ROOT=".."
 SEMU=${SEMU_ROOT}"/src/semu.py"
 ASM=${SEMU_ROOT}"/src/semuasm.py"
+WHWC=${SEMU_ROOT}"/src/semuwhwc.py"
 ROM=${SEMU_ROOT}"/roms/test_run.bin"
 
-function test_run {
-    py ${ASM} $1.sasm ${ROM}
-    if [[ $? != 0 ]]; then echo "Compilation failed for test case $1" && return 1; fi
+function test_run {                                                   
+
+    py ${ASM} `cat $1.tst` ${ROM}
+    if [[ $? != 0 ]]; then echo "Compilation failed for test case $1" && exit 1; fi
 
     py ${SEMU} ${ROM}    
-    if [[ $? != 0 ]]; then echo "Test failed (case $1)" && return 1; fi
+    if [[ $? != 0 ]]; then echo "Test failed (case $1)" && exit 1; fi
 }
 
-test_run consts || exit $?
-test_run locals || exit $?
+# Create definitions from hardware configuration
+py ${WHWC} > hw.sasm
+
+test_run consts
+test_run locals
 echo "Passed successfully"
 
 
