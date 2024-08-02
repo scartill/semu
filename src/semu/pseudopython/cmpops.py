1b11ee19d98e7b0149fe4d80eede1b4067facd25
Boris Resnick <boris.resnick@gmail.com>
2024-08-02 11:20:23 +0300

    Forgotten files

---
+++ src/semu/pseudopython/cmpops.py
@@ -0,0 +1,82 @@
+from dataclasses import dataclass
+from typing import Sequence
+
+from semu.pseudopython.flatten import flatten
+from semu.pseudopython.elements import Expression, Register
+
+
+class CompareOp:
+    def prepare(self, left: Register, right: Register):
+        raise NotImplementedError()
+
+
+class Eq(CompareOp):
+    pass
+
+
+class NotEq(CompareOp):
+    pass
+
+
+class Lt(CompareOp):
+    pass
+
+
+class LtE(CompareOp):
+    pass
+
+
+class Gt(CompareOp):
+    def prepare(self, left: Register, right: Register):
+        return [
+            '// Greater Than',
+            f'sub {left} {right} {left}',
+        ]
+
+
+class GtE(CompareOp):
+    pass
+
+
+@dataclass
+class Compare(Expression):
+    left: Expression
+    op: CompareOp
+    right: Expression
+
+    def __init__(self, target: Register, left: Expression, op: CompareOp, right: Expression):
+        super().__init__(target_type='bool32', target=target)
+        self.left = left
+        self.op = op
+        self.right = right
+
+    def emit(self) -> Sequence[str]:
+        available = self._get_available_registers([
+            self.target,
+            self.left.target,
+            self.right.target
+        ])
+
+        address = available.pop()
+        label_true = self._make_label()
+        label_false = self._make_label()
+        label_end = self._make_label()
+
+        return flatten([
+            '// Compare',
+            f'push {address}',
+            self.left.emit(),
+            self.right.emit(),
+            self.op.prepare(self.left.target, self.right.target),
+            f'ldr &{label_true} {address}',
+            f'jgt {self.left.target} {address}',
+            f'{label_false}:',
+            f'ldc 0 {self.target}',
+            f'ldr &{label_end} {address}',
+            f'jmp {address}',
+            f'{label_true}:',
+            f'ldc 1 {self.target}',
+            f'{label_end}:',
+            f'pop {address}',
+            '// End Compare'
+        ])
---
+++ src/semu/pseudopython/helpers.py
@@ -0,0 +1,143 @@
+import ast
+from typing import Sequence
+import logging as lg
+
+from semu.pseudopython.elements import Expression, Register, TargetType
+import semu.pseudopython.intops as intops
+import semu.pseudopython.boolops as boolops
+import semu.pseudopython.cmpops as cmpops
+
+
+def get_constant_type(ast_const: ast.Constant):
+    if isinstance(ast_const.value, bool):
+        return 'bool32'
+
+    if isinstance(ast_const.value, int):
+        return 'int32'
+
+    raise UserWarning(f'Unsupported constant type {type(ast_const.value)}')
+
+
+def int32const(ast_value: ast.AST):
+    if isinstance(ast_value, ast.Constant) and isinstance(ast_value.value, int):
+        value = ast_value.value
+
+        if value < 0 or value > 0xFFFFFFFF:
+            raise UserWarning(f'Int argument {ast_value} out of range')
+
+        return value
+    else:
+        raise UserWarning(f'Unsupported const int argument {ast_value}')
+
+
+def bool32const(ast_value: ast.AST):
+    if isinstance(ast_value, ast.Constant) and isinstance(ast_value.value, bool):
+        value = ast_value.value
+        return value
+    else:
+        raise UserWarning(f'Unsupported const int argument {ast_value}')
+
+
+def get_constant_value(target_type: TargetType, source: ast.AST):
+    if target_type == 'int32':
+        return int32const(source)
+
+    if target_type == 'bool32':
+        return bool32const(source)
+
+    raise UserWarning(f'Unsupported constant type {target_type}')
+
+
+def create_binop(left: Expression, right: Expression, op: ast.AST, target: Register):
+    required_type = None
+    Op = None
+
+    if isinstance(op, ast.Add):
+        required_type = 'int32'
+        Op = intops.Add
+
+    if isinstance(op, ast.Sub):
+        required_type = 'int32'
+        Op = intops.Sub
+
+    if isinstance(op, ast.Mult):
+        required_type = 'int32'
+        Op = intops.Mul
+
+    if Op is None:
+        raise UserWarning(f'Unsupported binop {op}')
+
+    if left.target_type != right.target_type:
+        raise UserWarning(f'Type mismatch {left.target_type} != {right.target_type}')
+
+    target_type = left.target_type
+
+    if target_type != required_type:
+        raise UserWarning(f'Unsupported binop type {target_type}')
+
+    return Op(target_type, target, left, right)
+
+
+def create_unary(right: Expression, op: ast.AST, target: Register):
+    required_type = None
+    Op = None
+
+    if isinstance(op, ast.Not):
+        required_type = 'bool32'
+        Op = boolops.Not
+
+    if isinstance(op, ast.USub):
+        required_type = 'int32'
+        Op = intops.Neg
+
+    if Op is None:
+        raise UserWarning(f'Unsupported unary op {op}')
+
+    target_type = right.target_type
+
+    if target_type != required_type:
+        raise UserWarning(f'Unsupported binop type {target_type}')
+
+    return Op(target_type, target, right)
+
+
+def create_boolop(args: Sequence[Expression], op: ast.AST, target: Register):
+    target_type = 'bool32'
+
+    for arg in args:
+        if arg.target_type != 'bool32':
+            raise UserWarning(f'Unsupported boolop type {arg.target_type}')
+
+    if isinstance(op, ast.And):
+        return boolops.And(target_type, target, args)
+
+    if isinstance(op, ast.Or):
+        return boolops.Or(target_type, target, args)
+
+    raise UserWarning(f'Unsupported boolop {op}')
+
+
+COMPARE_OPS = {
+    ast.Eq: cmpops.Eq,
+    ast.NotEq: cmpops.NotEq,
+    ast.Lt: cmpops.Lt,
+    ast.LtE: cmpops.LtE,
+    ast.Gt: cmpops.Gt,
+    ast.GtE: cmpops.GtE
+}
+
+
+def create_compare(left: Expression, ast_op: ast.AST, right: Expression, target: Register):
+    operand_type = left.target_type
+
+    if right.target_type != operand_type:
+        raise UserWarning(f'Unsupported compare type {right.target_type}')
+
+    Op = COMPARE_OPS.get(type(ast_op))  # type: ignore
+
+    if Op is None:
+        raise UserWarning(f'Unsupported compare op {ast_op}')
+
+    lg.debug(f'Creating compare {ast_op} {left} {right}')
+
+    return cmpops.Compare(target, left, Op(), right)
