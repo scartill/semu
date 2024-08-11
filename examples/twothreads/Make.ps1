71223d7ded3b056ff78fd81ca84f0b02bf479779
Boris Resnick <boris.resnick@gmail.com>
2024-08-12 00:44:59 +0300

    Reversed bad change

--- examples/twothreads/Make.ps1
+++ examples/twothreads/Make.ps1
@@ -2,7 +2,7 @@ $root = "$PSScriptRoot/../.."
 $assembler = "$root/src/semu/sasm/masm.py"
 
 $rom = "$root/roms/twothreads.bin"
-$kernel = "$root/lib/pseudopython/kernel"
+$kernel = "$root/lib/kernel"
 $app = "$PSScriptRoot/app.sasm"
 
 python $assembler --library $kernel $app $rom
