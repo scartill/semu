ee185f5d0553bf0c3fdd8e8126cf62960710679d
Boris Resnick <boris.resnick@gmail.com>
2024-08-23 08:21:40 +0300

    Testing WIP - 4 left

--- tests/testdata/pseudopython/localmembersetref.py
+++ tests/testdata/pseudopython/localmembersetref.py
@@ -9,7 +9,7 @@ c.b = True
 c.i = 42
 
 pc: ptr[C]
-pc = c
+pc = ref(c)
 
 assert_eq(bool_to_int(pc.b), 1)
 assert_eq(pc.i, 42)
@@ -22,6 +22,9 @@ change(pc, 101, False)
 assert_eq(bool_to_int(pc.b), 0)
 assert_eq(pc.i, 101)
 
-change(c, 404, True)
-assert_eq(bool_to_int(pc.b), 1)
-assert_eq(pc.i, 404)
+ppc: ptr[C]
+ppc = pc
+
+change(ppc, 404, True)
+assert_eq(bool_to_int(ppc.b), 1)
+assert_eq(ppc.i, 404)
